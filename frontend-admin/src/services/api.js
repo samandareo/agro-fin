a75f9c174ea31f7ba89f179377ec077f096d078b
samandareo <smdeveloper04@gmail.com>
2025-10-30 19:42:46 +0500

    fix: update API base URL to use the production endpoint

--- frontend-admin/src/services/api.js
+++ frontend-admin/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://fin.agrobank.uz/api/v1',
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://agro-fin.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
